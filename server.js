408a160b99da99fbb63b4616791cfeee5d82cf47
srdoyen <srdoyen@gmail.com>
2020-01-06 21:43:29 -0800

    sending data to server

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 const http = require("http");
 
+const todos = [
+  { id: 1, text: "Todo One" },
+  { id: 2, text: "Todo Two" },
+  { id: 3, text: "Todo Three" }
+];
+
 const server = http.createServer((req, res) => {
-  res.end();
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+    "X-Powered-By": "Node.js"
+  });
+
+  let body = [];
+
+  req
+    .on("data", chunk => {
+      body.push(chunk);
+    })
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+      console.log(body);
+    });
+
+  res.end(
+    JSON.stringify({
+      success: true,
+      data: todos
+    })
+  );
 });
 
 const PORT = 5000;
